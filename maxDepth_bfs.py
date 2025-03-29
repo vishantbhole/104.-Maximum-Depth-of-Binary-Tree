5f34ac232907dabc0f7d4f505e64cf3fd3aa059d
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-03-29 15:01:12 -0500

    Create maxDepth_bfs.py

---
+++ maxDepth_bfs.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def maxDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return 0
+        
+        level = 0
+        q = deque([root])
+
+        while q:
+            for i in range(len(q)):
+                root = q.popleft()
+                if root.left:
+                    q.append(root.left)
+                if root.right:
+                    q.append(root.right)
+            level += 1
+        return level        
